2f209f9c244ddb40429cf3bd8e404f7fa3e90ab1
goregath <goregath@github.com>
2023-03-10 21:53:24 +0100

    Change test names.

--- test/test-posix-split.lua
+++ test/test-posix-split.lua
@@ -7,44 +7,44 @@ group:setup(function()
 	return require "luawk.runtime.posix".new()
 end)
 
-group:add('split: defaults to FS="\\x20"', function(R)
+group:add('defaults to FS="\\x20"', function(R)
 	local a = {}
 	assert_equal(R.FS, "\32")
 	assert_equal(R.split("a b c", a), 3)
 	assert_equal(table.concat(a, ","), "a,b,c")
 end)
 
-group:add('split: special pattern of "\\x20" automatically trims leading and trailingspaces', function(R)
+group:add('special pattern of "\\x20" automatically trims leading and trailingspaces', function(R)
 	local a = {}
 	assert_equal(R.split("  a b c  ", a, "\x20"), 3)
 	assert_equal(table.concat(a, ","), "a,b,c")
 end)
 
-group:add('split: special pattern of "\\x20" automatically aggregate spaces', function(R)
+group:add('special pattern of "\\x20" automatically aggregate spaces', function(R)
 	local a = {}
 	assert_equal(R.split("a  b  c", a, "\x20"), 3)
 	assert_equal(table.concat(a, ","), "a,b,c")
 end)
 
-group:add("split: wide chars", function(R)
+group:add("wide chars", function(R)
 	local a = {}
 	assert_equal(R.split("ä ö ü", a, "\x20"), 3)
 	assert_equal(table.concat(a, "ß"), "äßößü")
 end)
 
-group:add("split: special null pattern splits every character", function(R)
+group:add("special null pattern splits every character", function(R)
 	local a = {}
 	assert_equal(R.split("abcäöü", a, ""), 6)
 	assert_equal(table.concat(a, ","), "a,b,c,ä,ö,ü")
 end)
 
-group:add("split: simple one-chacter pattern", function(R)
+group:add("simple one-chacter pattern", function(R)
 	local a = {}
 	assert_equal(R.split(",,a,b,c,", a, ","), 6)
 	assert_equal(table.concat(a, ";"), ";;a;b;c;")
 end)
 
-group:add("split: regular expression pattern pattern", function(R)
+group:add("regular expression pattern pattern", function(R)
 	local a = {}
 	assert_equal(R.split(",a,b,,,c,", a, ",+"), 5)
 	assert_equal(table.concat(a, ","), ",a,b,c,")
