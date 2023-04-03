1de8ce8f73c237907b9583e778e470720e5602f0
goregath <goregath@github.com>
2023-04-03 19:20:21 +0200

    Add pattern tests.

--- test/posix-getline.lua
+++ test/posix-getline.lua
@@ -28,4 +28,53 @@ group:add('cat', function(R)
 	assert_equal(table.concat(strbuf), file:read('*a'))
 end)
 
+group:add('set RS="" (special mode)', function(R)
+	local tmpfile = io.tmpfile()
+	local records = {}
+	tmpfile:write(
+		"\n",
+		"\n",
+		"abc\n",
+		"def\n",
+		"\n",
+		"\n",
+		"ghi\n",
+		"\n",
+		"\n"
+	)
+	tmpfile:seek("set")
+	R.RS = ""
+	for record in R.getline(tmpfile) do
+		table.insert(records, record)
+	end
+	assert_equal(#records, 2)
+	assert_equal(records[1], "abc\ndef")
+	assert_equal(records[2], "ghi")
+end)
+
+group:add('set RS="\\n\\n+"', function(R)
+	local tmpfile = io.tmpfile()
+	local records = {}
+	tmpfile:write(
+		"\n",
+		"\n",
+		"abc\n",
+		"def\n",
+		"\n",
+		"\n",
+		"ghi\n",
+		"\n",
+		"\n"
+	)
+	tmpfile:seek("set")
+	R.RS = "\n\n+"
+	for record in R.getline(tmpfile) do
+		table.insert(records, record)
+	end
+	assert_equal(#records, 3)
+	assert_equal(records[1], "")
+	assert_equal(records[2], "abc\ndef")
+	assert_equal(records[3], "ghi")
+end)
+
 group:run()
