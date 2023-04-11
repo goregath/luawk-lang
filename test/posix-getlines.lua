4f08c586d08ec9809ca1779ecfd66feb7fd839b6
goregath <goregath@github.com>
2023-04-12 00:26:14 +0200

    Fix test for getlines.

--- test/posix-getlines.lua
+++ test/posix-getlines.lua
@@ -6,7 +6,7 @@ package.path = string.format("%s/../src/?.lua;%s/lua/lib/?.lua;%s", path, path, 
 local assert_equal = require "assert".assert_equal
 local assert_true = require "assert".assert_true
 local assert_type = require "assert".assert_type
-local group = require "testgroup".new("luawk.runtime.posix getline()")
+local group = require "testgroup".new("luawk.runtime.posix getlines()")
 local iomock = {}
 
 function iomock.open(tbl)
@@ -21,88 +21,88 @@ group:setup(function()
 	return require "luawk.runtime.posix".new()
 end)
 
-group:add('getline from path', function(R)
-	local getline, state = R.getline(arg[0])
+group:add('getlines from path', function(R)
+	local getlines, state = R.getlines(arg[0])
 	R.RS = "\n"
-	assert_type(getline, "function")
+	assert_type(getlines, "function")
 	assert_type(state, "table")
-	assert_true(getline(state))
+	assert_true(getlines(state))
 end)
 
-group:add('getline from file handle', function(R)
+group:add('getlines from file handle', function(R)
 	local file = io.open(arg[0])
 	local record = file:read('l') file:seek('set')
-	local getline, state = R.getline(file)
+	local getlines, state = R.getlines(file)
 	R.RS = "\n"
-	assert_type(getline, "function")
+	assert_type(getlines, "function")
 	assert_type(state, "table")
-	assert_equal(getline(state), record)
+	assert_equal(getlines(state), record)
 end)
 
-group:add('getline from process handle', function(R)
+group:add('getlines from process handle', function(R)
 	local file = io.popen("echo 'record:eof'")
-	local getline, state = R.getline(file)
+	local getlines, state = R.getlines(file)
 	R.RS = ":"
-	assert_type(getline, "function")
+	assert_type(getlines, "function")
 	assert_type(state, "table")
-	assert_equal(getline(state), "record")
+	assert_equal(getlines(state), "record")
 end)
 
-group:add('getline from function', function(R)
+group:add('getlines from function', function(R)
 	local data = { "rec", "ord:eof" }
 	local function read()
 		return table.remove(data, 1)
 	end
-	local getline, state = R.getline(read)
+	local getlines, state = R.getlines(read)
 	R.RS = ":"
-	assert_type(getline, "function")
+	assert_type(getlines, "function")
 	assert_type(state, "table")
-	assert_equal(getline(state), "record")
+	assert_equal(getlines(state), "record")
 	assert_equal(#data, 0)
 end)
 
-group:add('getline from function (coroutine.wrap)', function(R)
+group:add('getlines from function (coroutine.wrap)', function(R)
 	local data = { "rec", "ord:eof" }
 	local read = coroutine.wrap(function()
 		for _, record in ipairs(data) do
 			coroutine.yield(record)
 		end
 	end)
-	local getline, state = R.getline(read)
+	local getlines, state = R.getlines(read)
 	R.RS = ":"
-	assert_type(getline, "function")
+	assert_type(getlines, "function")
 	assert_type(state, "table")
-	assert_equal(getline(state), "record" )
+	assert_equal(getlines(state), "record" )
 end)
 
-group:add('getline from coroutine', function(R)
+group:add('getlines from coroutine', function(R)
 	local data = { "rec", "ord:eof" }
 	local read = coroutine.create(function()
 		for _, record in ipairs(data) do
 			coroutine.yield(record)
 		end
 	end)
-	local getline, state = R.getline(read)
+	local getlines, state = R.getlines(read)
 	R.RS = ":"
-	assert_type(getline, "function")
+	assert_type(getlines, "function")
 	assert_type(state, "table")
-	assert_equal(getline(state), "record" )
+	assert_equal(getlines(state), "record" )
 end)
 
 
-group:add('getline from mocked file object', function(R)
+group:add('getlines from mocked file object', function(R)
 	local data = iomock.open { "rec", "ord:eof" }
-	local getline, state = R.getline(data)
+	local getlines, state = R.getlines(data)
 	R.RS = ":"
-	assert_type(getline, "function")
+	assert_type(getlines, "function")
 	assert_type(state, "table")
-	assert_equal(getline(state), "record")
+	assert_equal(getlines(state), "record")
 	assert_equal(#data, 0)
 end)
 
 group:add('cat file', function(R)
 	local strbuf = {}
-	for line, rt in R.getline(arg[0]) do
+	for line, rt in R.getlines(arg[0]) do
 		table.insert(strbuf, line)
 		table.insert(strbuf, rt)
 	end
@@ -124,7 +124,7 @@ group:add('set RS="" (special mode)', function(R)
 		"\n",
 	}
 	R.RS = ""
-	for record in R.getline(data) do
+	for record in R.getlines(data) do
 		table.insert(records, record)
 	end
 	assert_equal(#records, 2)
@@ -146,7 +146,7 @@ local records = {}
 		"\n",
 	}
 	R.RS = "\n\n+"
-	for record in R.getline(data) do
+	for record in R.getlines(data) do
 		table.insert(records, record)
 	end
 	assert_equal(#records, 3)
@@ -164,16 +164,16 @@ group:add('set RS="" (FS always matches "\\n")', function(R)
 		"y\n",
 		"z\n",
 	}
-	local getline, state = R.getline(data)
+	local getlines, state = R.getlines(data)
 	R.RS = ""
 	R.FS = "[ ,]+"
-	R[0] = getline(state)
+	R[0] = getlines(state)
 	assert_equal(#R, 6)
 	assert_equal(table.concat(R), "abcdef")
-	R[0] = getline(state)
+	R[0] = getlines(state)
 	assert_equal(#R, 3)
 	assert_equal(table.concat(R), "xyz")
-	assert_equal(getline(state), nil)
+	assert_equal(getlines(state), nil)
 end)
 
 group:run()
