da626210ac443c57d787b5e0d5f73ee4b7a0ff86
goregath <goregath@github.com>
2023-04-14 23:42:01 +0200

    Add compatibility module.

---
+++ src/luawk/compat53.lua
@@ -0,0 +1,48 @@
+--- Lua 5.3 compatibility module.
+-- @alias M
+-- @module compat53
+
+local version = tonumber((
+    assert(
+        _VERSION:sub(-3):match("5%.%d"),
+        "unable to parse lua version"
+    ):gsub("%D", "")
+))
+assert(
+    version >= 51 and version < 60,
+    "version mismatch, expected lua >= 5.1 and < 6.0"
+)
+
+local M = {
+    load = load,
+    utf8 = utf8
+}
+
+if version < 53 then
+    M.utf8 = {
+        charpattern =
+            version == 51
+            and "[%z\1-\127\194-\244][\128-\191]*"
+            or  "[\0-\127\194-\244][\128-\191]*"
+    }
+end
+
+if version == 51 then
+    local load = assert(load)
+    local loadstring = assert(loadstring)
+    local setfenv = assert(setfenv)
+    function M.load(chunk, chunkname, _, env)
+        local f, msg
+        if type(chunk) == "string" then
+            f, msg = loadstring(chunk, chunkname)
+        else
+            f, msg = load(chunk, chunkname)
+        end
+        if not f then
+            return nil, msg
+        end
+        return setfenv(f, env)
+    end
+end
+
+return M
--- src/luawk/utils.lua
+++ src/luawk/utils.lua
@@ -4,19 +4,8 @@
 
 local log = require 'luawk.log'
 
-local lua_version = _VERSION:sub(-3)
-local M = {
-    --- Character pattern matching UTF-8.
-    utf8charpattern = utf8.charpattern
-}
+local M = {}
 
-if lua_version == "5.1" then
-    M.utf8charpattern = "[%z\1-\127\194-\244][\128-\191]*"
-elseif lua_version == "5.2" then
-    M.utf8charpattern = "[\0-\127\194-\244][\128-\191]*"
-end
-
---- Print arguments to `io.stderr` using `string.format` and call `os.exit`(`1`).
 --  @param ... arguments for `string.format`
 function M.abort(...)
     io.stderr:write(string.format(...))
@@ -73,23 +62,6 @@ function M.trim(s)
     local _, i = string.find(s, '^[\32\t\n]*')
     local j = string.find(s, '[\32\t\n]*$')
     return string.sub(s, i + 1, j - 1)
-end
-
---- Compatibility layer setfenv() for Lua 5.2+.
---  Taken from Penlight Lua Libraries (lunarmodules/Penlight).
---  @function setfenv
-M.setfenv = _G.setfenv or function(f, t)
-    local var
-    local up = 0
-    repeat
-        up = up + 1
-        var = debug.getupvalue(f, up)
-    until var == '_ENV' or var == nil
-    if var then
-        debug.upvaluejoin(f, up, function() return var end, 1) -- use unique upvalue
-        debug.setupvalue(f, up, t)
-    end
-    if f ~= 0 then return f end
 end
 
 return M
