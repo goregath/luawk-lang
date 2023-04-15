b31ba1cd20c894cbc2bfabec9298e37cec0cc8b7
goregath <goregath@github.com>
2023-04-15 15:22:05 +0200

    Use /usr/bin/env to resolve lua.

--- test/bats/commons.bash
+++ test/bats/commons.bash
@@ -6,7 +6,7 @@ export BATS_TEST_TIMEOUT=1
 export LUA_PATH="../src/?.lua;$(lua -e 'print(package.path)')"
 
 luawk() {
-	timeout 3 lua -lluacov ../src/luawk.lua "$@"
+	timeout 3 /usr/bin/env lua -lluacov ../src/luawk.lua "$@"
 }
 
 cd "$BATS_TEST_DIRNAME"
