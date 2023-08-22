37cf16ec5b632df619b1cadcd168bae165e6f095
goregath <goregath@github.com>
2023-08-22 23:09:26 +0200

    TODO and explanation for supporting Lua <5.3

--- src/luawk/compat53.lua
+++ src/luawk/compat53.lua
@@ -2,6 +2,47 @@
 -- @alias M
 -- @module compat53
 
+-- TODO Write wrapper for bitlib when targeting lua <5.3
+
+-- EXCERPT FROM ERDE MANUAL
+-- ========================
+
+-- Compiling bitwise operators heavily depends on the Lua target. Erde uses
+-- the following table to determine how bit operations should be compiled:
+
+-- | Target | Compilation            | Erde  |               Lua              |
+-- |--------|------------------------|-------|:------------------------------:|
+-- | jit    | LuaBitOp               | 6 & 5 | require('bit').band(6, 5)      |
+-- | 5.1    | LuaBitOp               | 6 & 5 | require('bit').band(6, 5)      |
+-- | 5.1+   | Requires --bitlib flag | 6 & 5 | require('myBitLib').band(6, 5) |
+-- | 5.2    | bit32                  | 6 & 5 | require('bit32').band(6, 5)    |
+-- | 5.2+   | Requires --bitlib flag | 6 & 5 | require('myBitLib').band(6, 5) |
+-- | 5.3    | Native Syntax          | 6 & 5 | 6 & 5                          |
+-- | 5.3+   | Native Syntax          | 6 & 5 | 6 & 5                          |
+-- | 5.4    | Native Syntax          | 6 & 5 | 6 & 5                          |
+-- | 5.4+   | Native Syntax          | 6 & 5 | 6 & 5                          |
+
+-- You may also specify your own bit library using the --bitlib flag in the
+-- CLI. The library methods are assumed to be:
+
+-- | Syntax | Operator    | Method                     |
+-- |--------|-------------|----------------------------|
+-- | \|     | or          | require('myBitLib').bor    |
+-- | &      | and         | require('myBitLib').band   |
+-- | ~      | xor         | require('myBitLib').bxor   |
+-- | ~      | unary NOT   | require('myBitLib').bnot   |
+-- | >>     | right shift | require('myBitLib').rshift |
+-- | <<     | left shift  | require('myBitLib').lshift |
+
+-- CAUTION:
+-- -------
+
+-- > Trying to compile bitwise operators when targeting 5.1+ or 5.2+ requires
+-- > the use of --bitlib. This is because there really is no "sane" default
+-- > here. By far the most common bit libraries for Lua are LuaBitOp
+-- > (only works on 5.1 and 5.2) and bit32 (only works on 5.2), so it is left
+-- > to the developer to decide which library to use.
+
 local M = {
     load = load,
     utf8 = utf8
@@ -45,6 +86,7 @@ if version == 501 then
     end
 end
 
+M.version = string.format("%d.%d", major, minor)
 M.version_normalized = version
 M.version_major = major
 M.version_minor = minor
